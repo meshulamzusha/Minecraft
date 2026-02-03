af6015fcc98268c8a0d9b087072eca87617ee2cc
Your Name <you@example.com>
2026-02-03 17:53:11 +0200

    .

--- world.js
+++ world.js
@@ -1,39 +1,68 @@
-for (let index = 0; index < 608; index++) {
+for (let index = 0; index < 594; index++) {
     const box = document.createElement('div');
     box.classList.add('block')
 
-    if (index > 300 && index < 335) {
+    if (index > 310 && index < 345) {
         box.classList.add('grass')
         box.addEventListener('click', () => {
-            box.classList.replace('tree', 'sky')
+            box.classList.replace('grass', 'sky')
         })
-    } else if (index >= 335 && index < 420) {
+    } else if (index >= 345 && index < 400) {
         box.classList.add('dirt')
         box.addEventListener('click', () => {
-            box.classList.replace('tree', 'sky')
+            box.classList.replace('dirt', 'sky')
         })
-    } else if (index >= 420 && index < 608) {
+    } else if (index >= 400 && index < 594) {
         const random = Math.floor(Math.random() * 5);
         const stoneType = ['gold', 'red', 'blue', 'black', 'gray'][random]
 
         box.classList.add(stoneType)
         box.addEventListener('click', () => {
-            box.classList.replace('tree', 'sky')
+            box.classList.replace(stoneType, 'sky')
         })
     } else {
         box.classList.add('sky');
     }
 
     document.getElementById('world').appendChild(box);
+
 }
 
-console.log(document.getElementById('world').children.item(120));
+createTree(68)
+createTree(75)
+createTree(83)
+createTree(92)
+
+function createTree(firstBlock) {
+    const treeMap = [
+        [0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [0, 0, 2, 2, 0, 0],
+        [0, 0, 2, 2, 0, 0],
+        [0, 0, 2, 2, 0, 0],
+        [0, 0, 2, 2, 0, 0]
+    ]
 
 
-function renderTree(hight, width, treeTop) {
-    const blocks = document.getElementById('world')
+    for (let i = 0; i < treeMap[0].length; i++) {
+        for (let j = 0; j < treeMap.length * 33; j += 33) {
+            const block = document.getElementById('world').children[i + j + firstBlock]
 
-    for (let i = treeTop; i < treeTop + (hight * 32) ; i++) {
-        document.getElementById('world').children.replace()
-    } 
+            if (treeMap[j / 33][i] == 1) {
+                block.classList.replace('sky', 'leaves')
+                block.addEventListener('click', () => {
+                    block.classList.replace('leaves', 'sky')
+                })
+            }
+
+            if (treeMap[j / 33][i] == 2) {
+                block.classList.replace('sky', 'trunk')
+                block.addEventListener('click', () => {
+                    block.classList.replace('trunk', 'sky')
+                })
+            }
+        }
+    }
 }
