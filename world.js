21c895a6d34046a70bb59486fae81003709846f2
meshulamzusha <nauko763@gmail.com>
2026-02-04 07:43:34 +0200

    .

--- world.js
+++ world.js
@@ -1,37 +1,36 @@
-for (let index = 0; index < 594; index++) {
-    const box = document.createElement('div');
-    box.classList.add('block')
+function createGameWorld() {
+    for (let index = 0; index < 594; index++) {
+        const box = document.createElement('div');
+        box.classList.add('block')
 
-    if (index > 310 && index < 345) {
-        box.classList.add('grass')
-        box.addEventListener('click', () => {
-            box.classList.replace('grass', 'sky')
-        })
-    } else if (index >= 345 && index < 400) {
-        box.classList.add('dirt')
-        box.addEventListener('click', () => {
-            box.classList.replace('dirt', 'sky')
-        })
-    } else if (index >= 400 && index < 594) {
-        const random = Math.floor(Math.random() * 5);
-        const stoneType = ['gold', 'red', 'blue', 'black', 'gray'][random]
+        if (index > 310 && index < 345) {
+            box.classList.add('grass')
+            box.addEventListener('click', () => {
+                box.classList.replace('grass', 'sky')
+            })
+        } else if (index >= 345 && index < 400) {
+            box.classList.add('dirt')
+            box.addEventListener('click', () => {
+                box.classList.replace('dirt', 'sky')
+            })
+        } else if (index >= 400 && index < 594) {
+            const random = Math.floor(Math.random() * 5);
+            const stoneType = ['gold', 'red', 'blue', 'black', 'gray'][random]
 
-        box.classList.add(stoneType)
-        box.addEventListener('click', () => {
-            box.classList.replace(stoneType, 'sky')
-        })
-    } else {
-        box.classList.add('sky');
+            box.classList.add(stoneType)
+            box.addEventListener('click', () => {
+                box.classList.replace(stoneType, 'sky')
+            })
+        } else {
+            box.classList.add('sky');
+        }
+
+        document.getElementById('world').appendChild(box);
     }
-
-    document.getElementById('world').appendChild(box);
-
 }
 
-createTree(68)
-createTree(75)
-createTree(83)
-createTree(92)
+
+
 
 function createTree(firstBlock) {
     const treeMap = [
@@ -44,8 +43,8 @@ function createTree(firstBlock) {
         [0, 0, 2, 2, 0, 0],
         [0, 0, 2, 2, 0, 0]
     ]
-
-
+    
+    
     for (let i = 0; i < treeMap[0].length; i++) {
         for (let j = 0; j < treeMap.length * 33; j += 33) {
             const block = document.getElementById('world').children[i + j + firstBlock]
@@ -66,3 +65,23 @@ function createTree(firstBlock) {
         }
     }
 }
+
+
+function onToolSelected() {
+    const toolsCollection = document.getElementsByClassName('game-tool')
+    const tools = [...toolsCollection]
+
+    tools.forEach(tool => {
+        tool.addEventListener('click', (event) => {
+            console.log(event);
+            
+        })
+    })
+}
+
+onToolSelected()
+createGameWorld()
+createTree(68)
+createTree(75)
+createTree(83)
+createTree(92)
